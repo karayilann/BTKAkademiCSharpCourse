77fe9ee00038da46f57e3a709dd9da246303856f
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2024-09-29 00:26:35 +0300

    State Design Pattern Added

---
+++ BTK_C#/DesignPatterns/StateDesign/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+Context context = new Context();
+context.SetState(new ModifiedState());
+context.GetState().DoAction(context);
+
+DeletedState deletedState = new DeletedState();
+context.SetState(deletedState);
+deletedState.DoAction(context);
+
+interface IState
+{
+    void DoAction(Context context);
+
+}
+
+class ModifiedState :IState
+{
+    public void DoAction(Context context)
+    {
+        Console.WriteLine("State is Modified");
+    }
+}
+
+class DeletedState : IState
+{
+    public void DoAction(Context context)
+    {
+        Console.WriteLine("State is Deleted");
+    }
+}
+
+class AddedState : IState
+{
+    public void DoAction(Context context)
+    {
+        Console.WriteLine("State is Added");
+    }
+}
+
+internal class Context
+{
+    IState state;
+    public void SetState(IState state)
+    {
+        this.state = state;
+    }
+
+    public IState GetState()
+    {
+        return state;
+    }
+
+}
+
+
+
