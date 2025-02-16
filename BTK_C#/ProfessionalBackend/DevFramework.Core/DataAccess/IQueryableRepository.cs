7907472ba999ca3eed2091cd074fc8e9ef6181b6
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-02-16 23:46:58 +0300

    Added some interfaces

---
+++ BTK_C#/ProfessionalBackend/DevFramework.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,8 @@
+﻿using DevFramework.Core.Entities;
+
+namespace DevFramework.Core.DataAccess;
+
+public interface IQueryableRepository<T> where T : class, IEntity, new()
+{
+    IQueryable<T> Table { get; }
+}
