ad00b51b48a18a0b34d472bb27573d1274c92a28
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-02-17 02:42:50 +0300

    Create EfQueryableRepository.cs

---
+++ BTK_C#/ProfessionalBackend/DevFramework.Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.Entities;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EfQueryableRepository<T> :IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _dbContext;
+        private IDbSet<T> _entities;
+
+
+        public EfQueryableRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<T> Table => _entities;
+
+
+        protected virtual IDbSet<T> Entities
+        {
+            get
+            {
+                if (_entities == null)
+                {
+                    _entities = _dbContext.Set<T>();
+                }
+
+                return _entities;
+            }
+        }
+    }
+}
