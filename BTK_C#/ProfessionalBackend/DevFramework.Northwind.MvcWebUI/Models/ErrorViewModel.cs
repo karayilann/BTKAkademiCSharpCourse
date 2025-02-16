47dbcada33d0987e0d770e66b336db0d986001e1
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-02-16 21:16:13 +0300

    Changed ASP.NET Web Objects

--- BTK_C#/ProfessionalBackend/DevFramework.Northwind.MvcWebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DevFramework.Northwind.MvcWebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- BTK_C#/ProfessionalBackend/DevFramework.Northwind.WebApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DevFramework.Northwind.WebApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
